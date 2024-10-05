a31afdd9c2a6a67f9930c67d895a671bea4f9806
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-10-05 08:31:37 +0200

    find a way to deal with default parameters and random orders

---
+++ src/main.rs
@@ -0,0 +1,153 @@
+use std::marker::PhantomData;
+
+use typed_builder::TypedBuilder;
+
+#[derive(TypedBuilder, Debug)]
+// #[derive(Debug)]
+struct Pod<'a, T> {
+    first: u32,
+    second: &'a T,
+    #[builder(default)]
+    third: f32,
+}
+
+pub struct Assigned<T>(T);
+pub struct Empty<T>(PhantomData<T>);
+
+pub struct WithDefault<T>(T);
+
+trait AssignedOrDefault {
+    type ValueType;
+    fn value_or_default(self) -> Self::ValueType;
+}
+
+trait Assignable {
+    type ValueType;
+    fn assign(self, t: Self::ValueType) -> Assigned<Self::ValueType>;
+}
+
+impl<T> Assignable for Empty<T> {
+    type ValueType = T;
+    fn assign(self, t: T) -> Assigned<T> {
+        Assigned(t)
+    }
+}
+
+impl<T> Assignable for WithDefault<T> {
+    type ValueType = T;
+    fn assign(self, t: T) -> Assigned<T> {
+        Assigned(t)
+    }
+}
+
+impl<T> AssignedOrDefault for Assigned<T> {
+    type ValueType = T;
+
+    fn value_or_default(self) -> Self::ValueType {
+        self.0
+    }
+}
+
+impl<T> AssignedOrDefault for WithDefault<T> {
+    type ValueType = T;
+
+    fn value_or_default(self) -> Self::ValueType {
+        self.0
+    }
+}
+
+impl<T> Default for Empty<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+struct PodBuilder2<'a, T, State> {
+    state: State,
+    phantom: PhantomData<&'a T>,
+}
+
+impl<'a, T> PodBuilder2<'a, T, (Empty<u32>, Empty<&'a T>, WithDefault<f32>)> {
+    pub fn new() -> Self {
+        Self {
+            state: (
+                Empty::default(),
+                Empty::default(),
+                // todo we need a closure in here that sets the default value
+                WithDefault(Default::default()),
+            ),
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<'a, T, U, V, W> PodBuilder2<'a, T, (U, V, W)>
+where
+    U: Assignable<ValueType = u32>,
+{
+    pub fn first(self, first: u32) -> PodBuilder2<'a, T, (Assigned<u32>, V, W)> {
+        let state = (self.state.0.assign(first), self.state.1, self.state.2);
+        PodBuilder2 {
+            state,
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<'a, T, U, V, W> PodBuilder2<'a, T, (U, V, W)>
+where
+    V: Assignable<ValueType = &'a T>,
+{
+    pub fn second(self, second: &'a T) -> PodBuilder2<'a, T, (U, Assigned<&'a T>, W)> {
+        let state = (self.state.0, self.state.1.assign(second), self.state.2);
+        PodBuilder2 {
+            state,
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<'a, T, U, V, W> PodBuilder2<'a, T, (U, V, W)>
+where
+    W: Assignable<ValueType = f32>,
+{
+    pub fn third(self, third: f32) -> PodBuilder2<'a, T, (U, V, Assigned<W::ValueType>)> {
+        let state = (self.state.0, self.state.1, self.state.2.assign(third));
+        PodBuilder2 {
+            state,
+            phantom: Default::default(),
+        }
+    }
+}
+
+impl<'a, T, U, V, W> PodBuilder2<'a, T, (U, V, W)>
+where
+    U: AssignedOrDefault<ValueType = u32>,
+    V: AssignedOrDefault<ValueType = &'a T>,
+    W: AssignedOrDefault<ValueType = f32>,
+{
+    pub fn build(self) -> Pod<'a, T> {
+        Pod {
+            first: self.state.0.value_or_default(),
+            second: self.state.1.value_or_default(),
+            third: self.state.2.value_or_default(),
+        }
+    }
+}
+
+// #[inline(never)]
+// fn get_pod(x: u32, s: u32) -> Pod {
+//     let pod = Pod::builder().x(x).s(s).build();
+//     pod
+// }
+
+fn main() {
+    // let pod = get_pod(0xdead, 0xbeef);
+    // let pod = Pod { x: 313373, s: 1337 };
+    // let pod = Pod::builder().x(1).s(2).build();
+
+    let pod = PodBuilder2::new().first(1).second(&2).build();
+    let pod = PodBuilder2::new().second(&1).first(2).build();
+
+    // println!("{:?}", pod);
+}
